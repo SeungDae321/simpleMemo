b2b71e3f8eda9f54a8ea45fb1937ef4188f35abe
LeeSeungDae <seungdea33@gmail.com>
2023-06-20 13:12:24 +0900

    added delete func

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload=()=>{
-    for(let i = 1; i <= localStorage.length; i++){
+    for(let i = 0; i < localStorage.length; i++){
         const li = document.createElement('li');
-        li.innerText = localStorage.getItem(i);
+        li.innerText = localStorage.getItem(localStorage.key(i));
         list.appendChild(li);
-    }   
+    }
 }
 
 const textarea = document.querySelector('.textarea');
@@ -18,8 +18,8 @@ let date = new Date();
 createBtn.addEventListener('click', () => {
     if (title.value || textarea.value) {
       const memo = `${date.toLocaleString()} | ${title.value} - ${textarea.value}`;
-      const key = localStorage.length + 1; // 새로운 메모를 저장할 키 생성
-      localStorage.setItem(key, memo);
+      const key = localStorage.length; // 새로운 메모를 저장할 키 생성
+      localStorage.setItem(memo, memo);
       const li = document.createElement('li');
       li.innerText = memo;
       list.appendChild(li);
@@ -33,5 +33,11 @@ deleteBtn.addEventListener('click',()=>{
     list.innerHTML = "";
 })
 
-
-
+//delete
+list.addEventListener('click',(evt)=>{
+  const delconfirm = confirm('Do you want to delete this memo?');
+  if(delconfirm){
+    evt.target.remove();
+    localStorage.removeItem(evt.target.innerHTML);
+  }
+})
