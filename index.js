42c1802b9e2d2f0a85432393867ddfb3621b277e
LeeSeungDae <seungdea33@gmail.com>
2023-07-27 13:53:55 +0900

    added styles and seperate functions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 window.onload=()=>{
     for(let i = 0; i < localStorage.length; i++){
         const li = document.createElement('li');
-        li.innerText = localStorage.getItem(localStorage.key(i));
-        list.appendChild(li);
+        const pre = document.createElement('pre');
+        li.innerHTML = localStorage.getItem(localStorage.key(i));
+        pre.appendChild(li)
+        list.appendChild(pre);
     }
 }
 
@@ -12,32 +14,57 @@ const createBtn = document.querySelector('.createBtn');
 const deleteBtn = document.querySelector('.deleteBtn');
 const list = document.querySelector('.list');
 
+const viewMemo = document.querySelector('#view-memo');
+const writeMemo = document.querySelector('#write-memo');
 
-let date = new Date();
+
 
 createBtn.addEventListener('click', () => {
+  let date = new Date();
     if (title.value || textarea.value) {
-      const memo = `${date.toLocaleString()} | ${title.value} - ${textarea.value}`;
-      const key = localStorage.length; // 새로운 메모를 저장할 키 생성
+      const memo = `${date.toLocaleString()}<br><b>${title.value}</b><br><b><hr></b>${textarea.value}`;
       localStorage.setItem(memo, memo);
       const li = document.createElement('li');
-      li.innerText = memo;
-      list.appendChild(li);
+      const pre = document.createElement('pre')
+      li.innerHTML = memo;
+      pre.appendChild(li)
+      list.appendChild(pre);
       textarea.value = "";
       title.value = "";
+      document.querySelector('.createText').classList.toggle('disable');
+      document.querySelector('.viewList').classList.toggle('disable');
+      viewMemo.innerText = '메모 작성'
     }
   });
   
 deleteBtn.addEventListener('click',()=>{
+  const delconfirm = confirm('모든 메모를 삭제하겠습니까?');
+  if(delconfirm){
     localStorage.clear();
     list.innerHTML = "";
+  }
 })
 
 //delete
 list.addEventListener('click',(evt)=>{
-  const delconfirm = confirm('Do you want to delete this memo?');
+  const delconfirm = confirm('메모를 삭제 하겠습니까?');
   if(delconfirm){
     evt.target.remove();
     localStorage.removeItem(evt.target.innerHTML);
   }
 })
+
+
+
+viewMemo.addEventListener('click',()=>{
+  if(viewMemo.innerText === '메모 작성'){
+    viewMemo.innerText = '돌아가기'
+  } else if(viewMemo.innerText === '돌아가기')
+    {
+      viewMemo.innerText = '메모 작성'
+    }
+  document.querySelector('.createText').classList.toggle('disable');
+  document.querySelector('.viewList').classList.toggle('disable');
+})
+
+
